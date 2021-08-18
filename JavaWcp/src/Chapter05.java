6fac12fdc59faf8b47f3603df04aec109eb67407
sawabe-sawabe <sawabeyuunatsu@sawabeyuunatsunoMacBook-Pro.local>
2021-08-18 23:41:58 +0900

    practice05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,28 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		long long1 = 7263372036854775807L;
+		System.out.println(long1);
+
+		double double1 = 144444.89;
+		System.out.println(double1);
+
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		Float float1 = 0.456f;
+		System.out.println(float1);
+
+		Integer integer1 = 5555555;
+		System.out.println(integer1);
+
+		Character char1 = 'a';
+		System.out.println(char1);
+
+		int[] array1 = { 10, 20, 50, 60, 65 };
+		int int3 = array1[3];
+		System.out.println(int3);
+
+	}
+}
