f7d7ec050fba578376487e626a22704413f3f751
沢辺侑夏 <sawabeyuunatsu@sawabeyuunatsunoMacBook-Pro.local>
2021-08-18 23:00:05 +0900

    practice04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+
+		String name1 = "沢辺　侑夏";
+		System.out.println(name1);
+
+		name1 = "こんばんは！";
+		System.out.println(name1);
+
+		final int LUCKY_NUMBER = 10;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
